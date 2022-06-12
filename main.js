9548488930c516c76ce540f029fc1c11f8bd4994
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2022-06-12 18:10:27 -0400

    finished

--- main.js
+++ main.js
@@ -3,10 +3,12 @@
 document.querySelector(".toggle").addEventListener("click", toggle)
 
 function toggle (){
-    fetch('https://api.adviceslip.com')
+    fetch('https://api.adviceslip.com/advice')
   .then(response => response.json())
   .then(data => {
     console.log(data)
+    document.querySelector(".number").innerHTML = `# ${data.slip.id}`
+    document.querySelector(".advice").innerHTML = `"${data.slip.advice}"`
   });
 }
 
