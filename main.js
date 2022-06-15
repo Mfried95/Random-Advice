beb8a40a77add97ca6de6f643cf703b297e40a87
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2022-06-15 17:02:42 -0400

    Update main.js
    Added cache: "no-cache" to fetch api

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 document.querySelector(".toggle").addEventListener("click", toggle)
 
 function toggle (){
-    fetch('https://api.adviceslip.com/advice')
-  .then(response => response.json())
+    fetch('https://api.adviceslip.com/advice', {cache: "no-cache"})
+  .then(response => response.json() )
   .then(data => {
     console.log(data)
     document.querySelector(".number").innerHTML = `# ${data.slip.id}`
